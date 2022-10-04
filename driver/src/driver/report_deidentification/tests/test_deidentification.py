a9abc22f98cf5c59dcc79c02f422611db83e2953
tom <tom.young.316@gmail.com>
2022-10-04 12:41:07 +0100

    More exhaustive tests

--- driver/src/driver/report_deidentification/tests/test_deidentification.py
+++ driver/src/driver/report_deidentification/tests/test_deidentification.py
@@ -11,12 +11,35 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+import os
+from pathlib import Path
+from typing import Tuple
+
 from driver.report_deidentification import deidentify_text
+import pytest
+
+THIS_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
-def test_patient_name_is_redacted() -> None:
+def _patient_names_from_names_csv() -> list:
+    """From a csv file with a header and an index column extract name tuples"""
 
-    patient_name = "John Smith"
-    report_text = f"{patient_name} was x rayed and the prognosis is X"
+    def _tuple_from(line: str) -> Tuple[str, str]:
+        items = line.split(",")
+        return items[1], items[2]
 
-    assert patient_name not in deidentify_text(report_text)
+    path = THIS_DIR / "data" / "names.csv"
+
+    return [_tuple_from(line) for line in open(path, "r").readlines()[1:]]
+
+
+@pytest.mark.parametrize("full_name", _patient_names_from_names_csv())
+def test_patient_name_is_redacted(full_name: Tuple[str, str]) -> None:
+
+    first_name, last_name = full_name
+    report_text = f"{first_name} {last_name} was x rayed and the prognosis is X"
+    anon_text = deidentify_text(report_text)
+
+    for name in (first_name, last_name):
+        assert name not in anon_text
