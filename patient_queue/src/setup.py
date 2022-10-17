d6e6b397ceda52b77809ec6071be7ab34a267e4f
Anika C <anika.cawthorn@gmail.com>
2022-10-17 18:49:08 +0100

    Adding empty patient queue Python package.

---
+++ patient_queue/src/setup.py
@@ -0,0 +1,33 @@
+#  Copyright (c) 2022 University College London Hospitals NHS Foundation Trust
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from setuptools import find_packages, setup
+
+exec(open("patient_queue/_version.py").read())
+
+setup(
+    name="patient_queue",
+    version=__version__,  # noqa: F821
+    description="Service to manage queue for image and data download",
+    packages=find_packages(
+        include=[
+            "patient_queue*",
+        ],
+        exclude=[
+            "*tests",
+            "*.tests.*",
+        ],
+    ),
+    python_requires=">=3.10",
+)
