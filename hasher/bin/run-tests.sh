b221a13596058821a00eff289342938ea424502d
Nel Swanepoel <nels@users.noreply.github.com>
2022-10-19 15:11:05 +0100

    Small tweaks to make tests pass (#40)
    * Small tweaks to make tests pass
    
    Changed token_bucket run_tests script to be executable.
    Added type annotation to tokens functions to pass mypy inspection.
    Moved _bin_ dirs out of _src_ as original layout was a mistake on my part,
    Shortened comment lines that black doesn't want to format
    Added _token_bucket/tests/__init__.py_ which seems to be required by pytest
    
    * CI/CD tweaks for new bin layout
    
    * mypy added to token_buffer requirements
    
    * added `isort`

--- hasher/bin/run-tests.sh
+++ hasher/bin/run-tests.sh
@@ -18,18 +18,18 @@
 
 set -eo pipefail
 
-BIN_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-PACKAGE_DIR="${BIN_DIR%/*}"
-cd "$PACKAGE_DIR"
+BIN_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+HASHER_DIR="${BIN_DIR%/*}"
+cd $HASHER_DIR
 
-CONF_FILE=../../setup.cfg
+CONF_FILE=../setup.cfg
 
-mypy --config-file ${CONF_FILE} pixl_rd
+mypy --config-file ${CONF_FILE} src/hasher
 
-isort --settings-path ${CONF_FILE} pixl_rd
+isort --settings-path ${CONF_FILE} src/hasher
 
-black pixl_rd
+black src/hasher
 
 flake8 --config ${CONF_FILE}
 
-ENV=test pytest pixl_rd
+ENV=test pytest src/hasher/tests
--- token_buffer/src/token_buffer/tests/__init__.py
+++ token_buffer/src/token_buffer/tests/__init__.py
@@ -11,8 +11,3 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from pixl_rd.main import deidentify_text
-
-from ._version import __version__, __version_info__
-
-__all__ = ["deidentify_text"]
