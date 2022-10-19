9cac60391f60bcb08ba2e6e13c16deb0c2f6e98e
Anika C <anika.cawthorn@gmail.com>
2022-10-19 14:30:25 +0100

    Adding service check test.

--- patient_queue/src/patient_queue/tests/conftest.py
+++ patient_queue/src/patient_queue/tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 def env_sample_file():
     """generates EHR demographics file that is needed for renaming"""
     sample_file = "./.env.txt"
-    sample_entries = {"PULSAR_HTTP_PORT": 8080, "PULSAR_HTTP_PORT": 8081}
+    sample_entries = {"PULSAR_HTTP_PORT": 7071, "PULSAR_BINARY_PROTOCOL": 7072}
     with open(sample_file, 'w') as sfile:
         for k, v in sample_entries.items():
             sfile.write(f"{k} = {v}\n")
--- patient_queue/src/patient_queue/tests/tests_service.py
+++ patient_queue/src/patient_queue/tests/tests_service.py
@@ -1,6 +1,14 @@
+import requests
 
-def test_pulsar_up() -> None:
-    # needs PULSAR_HTTP_PORT -->  i.e. http://localhost:PULSAR_HTTP_PORT/metrics/
-    # if it is up and running, it gets a response 200 otherwise raises exception
-    """ Checks whether the Pulsar service is up and running by attempting to access metrics. """
+from patient_queue.utils import load_config_file
+
+
+def test_pulsar_up(env_sample_file) -> None:
+    """Checks whether the Pulsar service is up and running by attempting to access metrics. NOTE that docker
+    infrastructure (or at least Pulsar) needs to be up and running for this test to work."""
+    pulsar_port = load_config_file("PULSAR_HTTP_PORT", filename=env_sample_file).strip()
+    res = requests.get(url=f"http://localhost:{pulsar_port}/metrics/")
+    assert res.status_code == 200
+
+
 
