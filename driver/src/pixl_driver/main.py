c729e523afba3217daac0b88b7c9327d4d47ca20
tom <tom.young.316@gmail.com>
2022-10-27 09:09:34 +0100

    Logging

--- driver/src/pixl_driver/main.py
+++ driver/src/pixl_driver/main.py
@@ -1,18 +1,43 @@
 import click
 
+from pixl_driver._logging import logger, set_log_level
+
 
 @click.group()
 @click.option('--debug/--no-debug', default=False)
 def main(debug):
-    pass
+
+    set_log_level("INFO" if not debug else "DEBUG")
 
 
 @main.command()
 @click.argument("csv_filename", type=click.Path(exists=True))
-def up(csv_filename):
-    click.echo(f'Populating queue from {csv_filename}')
+@click.option("--pacs-rate", type=int, default=5,
+              help="Rate at which images are requested from PACS in images per second")
+@click.option("--ehr-rate", type=int, default=20,
+              help="Rate at which EHR is requested from EMAP in queries per second")
+def up(csv_filename: str, pacs_rate: int, ehr_rate: int) -> None:
+    logger.info(f'Populating queue from {csv_filename}. Using {pacs_rate} images/s '
+                f'and {ehr_rate} EHR queries/second')
+
+    # TODO: parse csv, add to queue, import token buckets
 
 
 @main.command()
-def stop():
-    click.echo('Stopping extraction')
+@click.option("--only-ehr", is_flag=True, show_default=True, default=False,
+              help="Only stop running EHR queries, leaving PACS to process")
+@click.option("--only-pacs", is_flag=True, show_default=True, default=False,
+              help="Only stop running PACS queries, leaving EHR to process")
+def stop(only_ehr: bool, only_pacs: bool) -> None:
+
+    if only_ehr and only_pacs:
+        raise ValueError("only-ehr and only-pacs arguments are mutually exclusive. "
+                         "Use only one")
+
+    stop_ehr = not only_pacs
+    stop_pacs = not only_ehr
+
+    logger.info(f"Stopping extraction of {'EHR' if stop_ehr else ''}"
+                f" {'PACS' if stop_pacs else ''}")
+
+    # TODO: set token buckets to zero
