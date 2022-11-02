f6646ec64bbea5dd4ff87934b1a3857517e4d730
Anika C <anika.cawthorn@gmail.com>
2022-11-02 23:49:30 +0000

    Adding some documentation on mode of operation RabbitMQ.

--- patient_queue/src/patient_queue/subscriber.py
+++ patient_queue/src/patient_queue/subscriber.py
@@ -1,3 +1,8 @@
+"""
+Basically subscriber wants to "wait" on queue and automatically when something arrives, consume the message. It seems
+that the other way round i.e. produce and then at some point connect & consume does not work with the code used.
+"""
+
 import pika
 
 
@@ -10,9 +15,11 @@ class PixlConsumer:
         self.channel.queue_declare(queue=_queue)
 
     def callback(self, body):
+        ### this needs the four parameters from the tutorial
         print(" [x] Received %r" % body)
 
     def retrieve_msg(self):
+        ### problem is that consumer needs to hang ...
         self.channel.basic_consume(queue=self.queue, on_message_callback=self.callback, auto_ack=True)
 
     def shutdown(self):
