1ce7e0758ebe01d5977d1d2fe86a793c2b20f05f
tom <tom.young.316@gmail.com>
2022-11-04 15:02:01 +0000

    Can't separate files

--- cli/src/pixl_cli/main.py
+++ cli/src/pixl_cli/main.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 import click
 import pika
+from requests import post
 from pixl_cli._logging import logger, set_log_level
 from pixl_cli._utils import clear_file, string_is_non_empty
 import yaml
@@ -89,6 +90,72 @@ def start() -> None:
 @cli.group()
 def update() -> None:
     """Update a consumer"""
+
+
+@start.command("ehr")
+@click.option(
+    "--rate",
+    type=int,
+    default=20,
+    help="Rate at which EHR is requested from EMAP in queries per second",
+)
+def start_ehr_extraction(rate: int) -> None:
+    """Start EHR extraction"""
+
+    if rate == 0:
+        raise RuntimeError("Cannot start EHR with extract rate of 0. Must be >0")
+
+    _update_ehr_extract_rate(rate)
+
+
+@update.command("ehr")
+@click.option(
+    "--rate",
+    type=int,
+    required=True,
+    help="Rate at which EHR is requested from EMAP in queries per second",
+)
+def update_ehr_rate(rate: int) -> None:
+    _update_ehr_extract_rate(rate)
+
+
+def _update_ehr_extract_rate(rate: int) -> None:
+    logger.info("Updating the EHR extraction rate")
+
+    base_url = f"http://{config['ehr_api']['host']}:{config['ehr_api']['port']}"
+    response = post(url=f"{base_url}/token-bucket-refresh-rate", json={"rate": rate})
+
+    if response.status_code == 200:
+        logger.info(
+            "Successfully updated EHR extraction, with a "
+            f"rate of {rate} queries/second"
+        )
+
+    else:
+        raise RuntimeError(f"Failed to start EHR extraction: {response}")
+
+
+@start.command("pacs")
+@click.option(
+    "--rate",
+    type=int,
+    default=5,
+    help="Rate at which images are requested from PACS in images per second",
+)
+def start_ehr_extraction(rate: int) -> None:
+    """Start PACS extraction"""
+    raise NotImplementedError
+
+
+@update.command("pacs")
+@click.option(
+    "--rate",
+    type=int,
+    required=True,
+    help="Rate at which images are requested from PACS in images per second",
+)
+def update_pacs_rate(rate: int) -> None:
+    raise NotImplementedError
 
 
 @cli.command()
