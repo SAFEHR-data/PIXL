63fd9a335362691c2a1478b55becfb651add8fb5
tom <tom.young.316@gmail.com>
2022-11-04 13:11:09 +0000

    Add start/stop

--- cli/src/pixl_cli/main.py
+++ cli/src/pixl_cli/main.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 import click
 import pika
+from requests import post
 from pixl_cli._logging import logger, set_log_level
 import yaml
 
@@ -31,7 +32,7 @@ config = _load_config()
 @click.option("--debug/--no-debug", default=False)
 def cli(debug: bool) -> None:
     """Main CLI entrypoint"""
-    set_log_level("INFO" if not debug else "DEBUG")
+    set_log_level("WARNING" if not debug else "DEBUG")
 
 
 @cli.command()
@@ -85,6 +86,11 @@ def start() -> None:
     """Start a consumer"""
 
 
+@cli.group()
+def update() -> None:
+    """Update a consumer"""
+
+
 @start.command()
 @click.option(
     "--rate",
@@ -97,16 +103,59 @@ def pacs(rate: int) -> None:
     raise NotImplementedError
 
 
+@update.command()
+@click.option(
+    "--rate",
+    type=int,
+    required=True,
+    help="Rate at which images are requested from PACS in images per second",
+)
+def pacs(rate: int) -> None:
+    raise NotImplementedError
+
+
 @start.command()
 @click.option(
-    "--ehr-rate",
+    "--rate",
     type=int,
     default=20,
     help="Rate at which EHR is requested from EMAP in queries per second",
 )
 def ehr(rate: int) -> None:
     """Start EHR extraction"""
-    raise NotImplementedError
+
+    if rate == 0:
+        raise RuntimeError("Cannot start EHR with extract rate of 0. Must be >0")
+
+    _update_ehr_extract_rate(rate)
+
+
+@update.command()
+@click.option(
+    "--rate",
+    type=int,
+    required=True,
+    help="Rate at which EHR is requested from EMAP in queries per second",
+)
+def ehr(rate: int) -> None:
+    _update_ehr_extract_rate(rate)
+
+
+def _update_ehr_extract_rate(rate: int) -> None:
+    logger.info("Updating the EHR extraction rate")
+
+    base_url = f"http://{config['ehr_api']['host']}:{config['ehr_api']['port']}"
+    response = post(
+        url=f"{base_url}/token-bucket-refresh-rate",
+        json={"rate": rate}
+    )
+
+    if response.status_code == 200:
+        logger.info("Successfully updated EHR extraction, with a "
+                    f"rate of {rate} queries/second")
+
+    else:
+        raise RuntimeError(f"Failed to start EHR extraction: {response}")
 
 
 @cli.command()
