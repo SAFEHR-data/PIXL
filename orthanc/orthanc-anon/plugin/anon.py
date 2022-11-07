8da6094e204d31fbcafb195ddc4d13e8198f4b6d
bathomas <bathomas@users.noreply.github.com>
2022-11-07 14:58:03 +0000

    [WIP] stub for POST refresh Azure DICOM token

--- orthanc/orthanc-anon/plugin/anon.py
+++ orthanc/orthanc-anon/plugin/anon.py
@@ -1,3 +1,17 @@
+#  Copyright (c) 2022 University College London Hospitals NHS Foundation Trust
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from decouple import config
 from io import BytesIO
 
 from pydicom import dcmread, dcmwrite
@@ -6,11 +20,37 @@ from pydicom.filebase import DicomFileLike
 import hashlib
 import orthanc
 import pprint
+import threading
 import yaml
 
 import logging 
 
 import pixl_dcmd
+
+def AzureDICOMTokenRefresh():
+    global TIMER
+    TIMER = None
+
+    orthanc.LogWarning("Refreshing Azure DICOM token")
+
+    AZ_DICOM_TOKEN_REFRESH_SECS = int(config('AZ_DICOM_TOKEN_REFRESH_SECS'))
+    AZ_DICOM_ENDPOINT_CLIENT_ID = config('AZ_DICOM_ENDPOINT_CLIENT_ID')
+    AZ_DICOM_ENDPOINT_CLIENT_SECRET = config('AZ_DICOM_ENDPOINT_CLIENT_SECRET')
+    AZ_DICOM_ENDPOINT_TENANT_ID = config('AZ_DICOM_ENDPOINT_TENANT_ID')
+
+    #res = requests.post()
+    TIMER = threading.Timer(AZ_DICOM_TOKEN_REFRESH_SECS, AzureDICOMTokenRefresh)
+    TIMER.start()
+
+def OnChange(changeType, level, resource):
+    if changeType == orthanc.ChangeType.ORTHANC_STARTED:
+        orthanc.LogWarning("Starting the scheduler")
+        AzureDICOMTokenRefresh()
+
+    elif changeType == orthanc.ChangeType.ORTHANC_STOPPED:
+        if TIMER != None:
+            orthanc.LogWarning("Stopping the scheduler")
+            TIMER.cancel()
 
 def ReceivedInstanceCallback(receivedDicom, origin):
     """Modifies a DICOM instance received by Orthanc and applies anonymisation."""
@@ -43,3 +83,4 @@ def ReceivedInstanceCallback(receivedDicom, origin):
     return orthanc.ReceivedInstanceAction.MODIFY, pixl_dcmd.write_dataset_to_bytes(dataset)
 
 orthanc.RegisterReceivedInstanceCallback(ReceivedInstanceCallback)
+orthanc.RegisterOnChangeCallback(OnChange)
