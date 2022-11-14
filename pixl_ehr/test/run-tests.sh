08f926421b4a573400439a25ead1dadf485afdd4
tom <tom.young.316@gmail.com>
2022-11-14 12:05:35 +0000

    Renaming

--- pixl_ehr/test/run-tests.sh
+++ pixl_ehr/test/run-tests.sh
@@ -17,5 +17,5 @@ set -euxo pipefail
 BIN_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 PACKAGE_DIR="${BIN_DIR%/*}"
 
-./"${PACKAGE_DIR}"/test/run-lint-and-unit-tests.sh
-./"${PACKAGE_DIR}"/test/run-system-tests.sh
+. "${PACKAGE_DIR}"/test/run-lint-and-api-tests.sh
+. "${PACKAGE_DIR}"/test/run-processing-tests.sh
