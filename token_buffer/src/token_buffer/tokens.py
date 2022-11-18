3065733643b88eb63aaeb4a650e8305702792e4d
tom <tom.young.316@gmail.com>
2022-11-18 08:45:54 +0000

    Use derived class, fix import, allow zero rate

--- token_buffer/src/token_buffer/tokens.py
+++ token_buffer/src/token_buffer/tokens.py
@@ -11,42 +11,52 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
 import token_bucket as tb
 
 
-def create_token_bucket(_rate: int = 5, _capacity: int = 5) -> tb.Limiter:
-    """Creates a token bucket for rate limitation
-
-    Uses the token bucket implementation from `Flaconry`
-    <https://github.com/falconry/token-bucket> to limit access rates for downloading
-    images from PACS/VNA. During discussion with the UCLH imaging team, the optimal
-    rate limit was determined as 5 images per second for now. However, there might be
-    still situations where this is too much which is why _rate and _capacity have been
-    provided as parameters so that they can change.
-
-    :param _rate: the number of tokens added per second
-    :param _capacity: the maximum number of tokens in the bucket at any one point in
-                      time
-
-    :returns: a Limiter object
+class TokenBucket(tb.Limiter):
     """
-    return tb.Limiter(rate=_rate, capacity=_capacity, storage=tb.MemoryStorage())
-
-
-def get_token(token_bucket: tb.Limiter) -> bool:
-    """Attempts to retrieve token from exising bucket.
-
     Rate limitation is governed by the existence of tokens in a bucket, whereby the
-    bucket is refilled every second. As long as a token can be retrieved, an image can
-    be downloaded from PACS/VNA. Should there be no more tokens inside the bucket, the
-    image request is added back into the queue. Note that the Limiter object can operate
-    the rate on different "streams", which are specified by a string object, also called
-    key. This key has been hard coded here to "pixl" as we do not expect more than one
-    streams at this point in time.
-
-    :param token_bucket: the token bucket that limits the rate for the download and
-    which has been created beforehand :returns: True if a token could be removed,
-    otherwise false
+    bucket is refilled every second. As long as a token can be retrieved, an item can
+    be downloaded. Should there be no more tokens inside the bucket, the request is
+    added back into the queue. Note that the Limiter object can operate the rate on
+    different "streams", which are specified by a string object, also called key. This
+    key has been hard coded here to "pixl" as we do not expect more than one streams at
+    this point in time.
     """
-    return bool(token_bucket.consume("pixl"))
+
+    key = b"pixl"
+
+    def __init__(
+        self,
+        rate: int = 5,
+        capacity: int = 5,
+        storage: tb.StorageBase = tb.MemoryStorage(),
+    ):
+        """
+        Uses the token bucket implementation from `Flaconry`
+        <https://github.com/falconry/token-bucket> to limit access rates for downloading
+        /extracting images where throttling is required.
+
+        :param rate: The number of tokens added per second
+        :param capacity: The maximum number of tokens in the bucket at any point in time
+        :param storage: Type of storage used to hold the tokens
+        """
+
+        self._zero_rate = False
+
+        if rate == 0:
+            rate = 1  # tb.Limiter does not allow zero rates, so keep track...
+            self._zero_rate = True
+
+        super().__init__(rate=rate, capacity=capacity, storage=storage)
+
+    @property
+    def has_token(self) -> bool:
+        """Does this token bucket have a token?"""
+        return not self._zero_rate and bool(self.consume(self.key))
+
+    @property
+    def rate(self) -> int:
+        """Rate in items per second"""
+        return 0 if self._zero_rate else int(self._rate)
