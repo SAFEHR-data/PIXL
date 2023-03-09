ba8caa3245977f74bbf08ac284bbe8e4938850f5
tom <tom.young.316@gmail.com>
2023-03-09 18:17:04 +0000

    Add scripts

---
+++ scripts/cmove_all_studies.py
@@ -0,0 +1,85 @@
+import os
+import requests
+import argparse
+
+from json import JSONDecodeError
+from typing import Any, List, Optional
+from datetime import datetime, timedelta
+from requests.auth import HTTPBasicAuth
+
+os.environ["NO_PROXY"] = os.environ["no_proxy"] = "localhost"
+
+
+class Orthanc:
+    def __init__(
+        self,
+        url=f"http://localhost:{os.environ['ORTHANC_PORT']}",
+        username=os.environ["ORTHANC_USERNAME"],
+        password=os.environ["ORTHANC_PASSWORD"],
+        anon_aet=os.environ["ORTHANC_ANON_AE_TITLE"]
+    ):
+        self._url = url.rstrip("/")
+        self._username = username
+        self._password = password
+        self._anon_aet = anon_aet
+
+        self._auth = HTTPBasicAuth(username=username, password=password)
+
+    def cmove_to_anon(self, _query_id: str) -> None:
+        data = {"TargetAet": self._anon_aet}
+        print(self._post(f"/queries/{_query_id}/retrieve", data=data))
+
+    def query_remote(self, data: dict) -> str:
+        return self._post("/modalities/PIXL-Raw/query", data=data)["ID"]
+
+    def _post(self, path: str, data: dict) -> Any:
+        return _deserialise(
+            requests.post(f"{self._url}{path}", json=data, auth=self._auth)
+        )
+
+
+def _deserialise(response: requests.Response) -> Any:
+    """Decode an Orthanc rest API response"""
+
+    if response.status_code != 200:
+        raise requests.HTTPError(
+            f"Failed request. "
+            f"Status code: {response.status_code}"
+            f"Content: {response.content.decode()}"
+        )
+    try:
+        return response.json()
+    except (JSONDecodeError, ValueError):
+        raise requests.HTTPError(f"Failed to parse {response} as json")
+
+
+def parse_args() -> argparse.Namespace:
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "start_date",
+        help="Date from which to trigger C-Move from in the format: YYYY-MM-DD"
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    orthanc = Orthanc()
+
+    start_date = datetime.fromisoformat(args.start_date)
+    now = datetime.now()
+
+    for i in range((now - start_date).days):
+        date = start_date + timedelta(days=i)
+
+        query_id = orthanc.query_remote(
+            data={"Level": "Study",
+                  "Query": {"StudyDate": date.strftime('%Y%m%d')}}
+        )
+        print(f"Driving C-Move for study {i} {query_id} on {date}")
+
+        orthanc.cmove_to_anon(query_id)
+
+        _ = input("Waiting... press any key to continue")
---
+++ scripts/filter_cohort_for_those_present_in_raw.py
@@ -0,0 +1,87 @@
+import os
+import requests
+import sys
+
+from json import JSONDecodeError
+from typing import Any, List
+from requests.auth import HTTPBasicAuth
+
+os.environ["NO_PROXY"] = os.environ["no_proxy"] = "localhost"
+
+
+class Study:
+    def __init__(self, uid: str):
+        self.uid = uid
+
+
+class Orthanc:
+    def __init__(
+        self,
+        url=f"http://localhost:{os.environ['ORTHANC_PORT']}",
+        username=os.environ["ORTHANC_USERNAME"],
+        password=os.environ["ORTHANC_PASSWORD"],
+    ):
+        self._url = url.rstrip("/")
+        self._username = username
+        self._password = password
+
+        self._auth = HTTPBasicAuth(username=username, password=password)
+
+    @property
+    def studies(self) -> List[Study]:
+        """Get all the studies in an Orthanc instance"""
+        uids = self.query_local(
+            {
+                "Level": "Study",
+                "Query": {},
+            }
+        )
+        return [Study(uid) for uid in uids]
+
+    def accession_number(self, study: Study) -> str:
+        data = self._get(f"/studies/{study.uid}")
+        return data["MainDicomTags"]["AccessionNumber"]
+
+    def query_local(self, data: dict) -> Any:
+        return self._post("/tools/find", data=data)
+
+    def _get(self, path: str) -> Any:
+        return _deserialise(requests.get(f"{self._url}{path}", auth=self._auth))
+
+    def _post(self, path: str, data: dict) -> Any:
+        return _deserialise(
+            requests.post(f"{self._url}{path}", json=data, auth=self._auth)
+        )
+
+
+def _deserialise(response: requests.Response) -> Any:
+    """Decode an Orthanc rest API response"""
+
+    if response.status_code != 200:
+        raise requests.HTTPError(
+            f"Failed request. "
+            f"Status code: {response.status_code}"
+            f"Content: {response.content.decode()}"
+        )
+    try:
+        return response.json()
+    except (JSONDecodeError, ValueError):
+        raise requests.HTTPError(f"Failed to parse {response} as json")
+
+
+if __name__ == "__main__":
+
+    filename = sys.argv[1]
+    orthanc = Orthanc()
+
+    present_accession_numbers = [orthanc.accession_number(s) for s in orthanc.studies]
+    print(f"Found {len(present_accession_numbers)} total studies")
+
+    with open(filename, "r") as file:
+        with open(f"{filename.rstrip('.csv')}_filtered.csv", "w") as new_file:
+            for line in file:
+
+                if any(a in line for a in present_accession_numbers):
+                    continue
+
+                print(line, file=new_file)
