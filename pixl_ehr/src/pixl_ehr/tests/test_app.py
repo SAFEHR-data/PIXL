6806127d61f7dce106dfd8af53733e31dcb17ea1
tom <tom.young.316@gmail.com>
2023-10-11 20:07:55 +0100

    fix ci

--- pixl_ehr/src/pixl_ehr/tests/test_app.py
+++ pixl_ehr/src/pixl_ehr/tests/test_app.py
@@ -16,13 +16,8 @@ This file contains unit tests for the API that do not require any test services
 """
 from fastapi.testclient import TestClient
 from pixl_ehr.main import AppState, app, state
-from requests import Response
 
 client = TestClient(app)
-
-
-def _is_error(response: Response) -> bool:
-    return bool(400 <= int(response.status_code) < 450)
 
 
 def test_heartbeat_response_is_200() -> None:
@@ -37,13 +32,13 @@ def test_initial_state_has_no_token() -> None:
 def test_updating_the_token_refresh_rate_to_negative_fails() -> None:
 
     response = client.post("/token-bucket-refresh-rate", json={"rate": -1})
-    assert _is_error(response)
+    assert response.is_error
 
 
 def test_updating_the_token_refresh_rate_to_string_fails() -> None:
 
     response = client.post("/token-bucket-refresh-rate", json={"rate": "a string"})
-    assert _is_error(response)
+    assert response.is_error
 
 
 def test_updating_the_token_refresh_rate_updates_state() -> None:
