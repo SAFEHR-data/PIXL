2e60a5085a0150d8a2c715fc86b4d7f25feb774b
Milan Malfait <m.malfait@ucl.ac.uk>
2023-11-29 18:16:41 +0000

    `pixl_core`: fix test checking datetime
    Fixing ruff rule DTZ007 in e96307b caused this test to fail because the timestamp didn't include the timezone.
    Also requires `import datetime` to access the `timezone` attribute

--- pixl_core/tests/patient_queue/test_utils.py
+++ pixl_core/tests/patient_queue/test_utils.py
@@ -11,8 +11,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import datetime
 import json
-from datetime import datetime as dt
 
 from core.patient_queue.utils import deserialise, serialise
 
@@ -22,14 +22,13 @@ def test_serialise() -> None:
     msg_body = serialise(
         mrn="111",
         accession_number="123",
-        study_datetime=dt.strptime("Nov 22 2022 1:33PM", "%b %d %Y %I:%M%p").replace(
-            tzinfo=dt.timezone.utc
-        ),
+        study_datetime=datetime.datetime.strptime(
+            "Nov 22 2022 1:33PM", "%b %d %Y %I:%M%p"
+        ).replace(tzinfo=datetime.timezone.utc),
     )
     assert (
-        msg_body.decode()
-        == '{"mrn": "111", "accession_number": "123",\
-            "study_datetime": "2022-11-22T13:33:00"}'
+        msg_body.decode() == '{"mrn": "111", "accession_number": "123", '
+            '"study_datetime": "2022-11-22T13:33:00+00:00"}'
     )
 
 
@@ -40,6 +39,6 @@ def test_simple_deserialise() -> None:
 
 def test_deserialise_datetime() -> None:
     """Checks that datetimes can be correctly serialised"""
-    timestamp = dt.fromordinal(100012)
+    timestamp = datetime.datetime.fromordinal(100012)
     data = deserialise(serialise(mrn="", accession_number="", study_datetime=timestamp))
     assert data["study_datetime"] == timestamp
