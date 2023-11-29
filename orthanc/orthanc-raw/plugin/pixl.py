7c36eb79c0ebe6c413b32a043097631c251b4e7b
ruaridhg <ruaridhg@users.noreply.github.com>
2023-11-29 16:21:53 +0000

    Fix ruff rules in orthanc-raw - add docstrings

--- orthanc/orthanc-raw/plugin/pixl.py
+++ orthanc/orthanc-raw/plugin/pixl.py
@@ -1,3 +1,12 @@
+"""
+
+Facilitates routing of stable studies from orthanc-raw to orthanc-anon
+
+This module provides:
+-OnChange: route stable studies and if auto-routing enabled
+-ShouldAutoRoute: checks whether auto-routing is enabled
+-OnHeartBeat: extends the REST API
+
 #  Copyright (c) 2022 University College London Hospitals NHS Foundation Trust
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,25 +20,35 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+"""
 import os
 
 import orthanc
 
 
-def OnChange(changeType, level, resourceId):
+def OnChange(changeType, level, resourceId): # noqa: ARG001
+    """
     # Taken from:
     # https://book.orthanc-server.com/plugins/python.html#auto-routing-studies
+    This routes any stable study to a modality named PIXL-Anon if
+    ShouldAutoRoute returns true
+    """
     if changeType == orthanc.ChangeType.STABLE_STUDY and ShouldAutoRoute():
-        print("Stable study: %s" % resourceId)
+        print("Stable study: %s" % resourceId) # noqa: T201
         orthanc.RestApiPost("/modalities/PIXL-Anon/store", resourceId)
 
 
-def OnHeartBeat(output, uri, **request):
+def OnHeartBeat(output, uri, **request): # noqa: ARG001
+    """Extends the REST API by registering a new route in the REST API"""
     orthanc.LogWarning("OK")
     output.AnswerBuffer("OK\n", "text/plain")
 
 
 def ShouldAutoRoute():
+    """
+    Checks whether ORTHANC_AUTOROUTE_RAW_TO_ANON environment variable is
+    set to true or false
+    """
     return os.environ.get("ORTHANC_AUTOROUTE_RAW_TO_ANON", "false").lower() == "true"
 
 
