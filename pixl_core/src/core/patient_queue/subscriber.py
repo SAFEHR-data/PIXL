511df0771df369d0c07e1d4b10dca95249aad4d4
Stef Piatek <s.piatek@ucl.ac.uk>
2023-12-11 15:33:59 +0000

    Limit rabbitmq prefetch and use default rabbitmq
    Shouldn't need to increase the timeout for rabbitmq

--- pixl_core/src/core/patient_queue/subscriber.py
+++ pixl_core/src/core/patient_queue/subscriber.py
@@ -47,6 +47,8 @@ class PixlConsumer(PixlQueueInterface):
         """Establishes connection to queue."""
         self._connection = await aio_pika.connect_robust(self._url)
         self._channel = await self._connection.channel()
+        # Only prefetch 10 messages from the queue
+        await self._channel.set_qos(prefetch_count=10)
         self._queue = await self._channel.declare_queue(self.queue_name)
         return self
 
