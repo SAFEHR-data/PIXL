53adabd50a76ca2ef4369f633c5e653f8e94730e
Jeremy Stein <j.stein@ucl.ac.uk>
2023-12-13 17:16:16 +0000

    Be consistent and safe in our bash options.

--- test/scripts/check_entry_in_pixl_anon.sh
+++ test/scripts/check_entry_in_pixl_anon.sh
@@ -12,7 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-set -eux pipefail
+set -euxo pipefail
 
 _sql_command="select * from emap_data.ehr_anon"
 _result=$(docker exec -it test-postgres-1 /bin/bash -c \
