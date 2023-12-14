228d46e817e679092e98294b3bb268bce8ece07b
Jeremy Stein <j.stein@ucl.ac.uk>
2023-12-14 17:33:41 +0000

    Be consistent and safe in our bash options. (#184)
    The pipefail option was clearly intended but was not being applied in a lot of cases.
    DB creation code was wrong; use the postgres docker image's env var intended for this purpose instead.

--- test/scripts/check_max_storage_in_orthanc_raw.sh
+++ test/scripts/check_max_storage_in_orthanc_raw.sh
@@ -12,7 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-set -eux pipefail
+set -euxo pipefail
 
 # This could be much improved by having more realistic test data some of
 # which actually was persisted
