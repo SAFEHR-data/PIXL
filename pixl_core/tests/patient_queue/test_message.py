558f69467243df8f819c626fac145d2d453978ce
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-19 14:46:55 +0000

    Use new classes in message testing

--- pixl_core/tests/patient_queue/test_message.py
+++ pixl_core/tests/patient_queue/test_message.py
@@ -14,23 +14,26 @@
 import datetime
 import json
 
-from core.patient_queue.message import deserialise, serialise
+from core.patient_queue.message import Message, SerialisedMessage
 
 
 def test_serialise() -> None:
     """Checks that messages can be correctly serialised"""
-    msg_body = serialise(
-        mrn="111",
-        accession_number="123",
-        study_datetime=datetime.datetime.strptime("Nov 22 2022 1:33PM", "%b %d %Y %I:%M%p").replace(
-            tzinfo=datetime.timezone.utc
-        ),
-        procedure_occurrence_id="234",
-        project_name="test project",
-        omop_es_timestamp=datetime.datetime.strptime(
-            "Dec 7 2023 2:08PM", "%b %d %Y %I:%M%p"
-        ).replace(tzinfo=datetime.timezone.utc),
+    msg = Message(
+        {
+            "mrn": "111",
+            "accession_number": "123",
+            "study_datetime": datetime.datetime.strptime(
+                "Nov 22 2022 1:33PM", "%b %d %Y %I:%M%p"
+            ).replace(tzinfo=datetime.timezone.utc),
+            "procedure_occurrence_id": "234",
+            "project_name": "test project",
+            "omop_es_timestamp": datetime.datetime.strptime(
+                "Dec 7 2023 2:08PM", "%b %d %Y %I:%M%p"
+            ).replace(tzinfo=datetime.timezone.utc),
+        }
     )
+    msg_body = msg.serialise()
     assert (
         msg_body.decode() == '{"mrn": "111", "accession_number": "123", '
         '"study_datetime": "2022-11-22T13:33:00+00:00", '
@@ -42,20 +45,23 @@ def test_serialise() -> None:
 
 def test_simple_deserialise() -> None:
     """Checks a simple JSON deserialise works"""
-    assert deserialise((json.dumps({"key": "value"})).encode("utf-8"))["key"] == "value"
+    serialised_msg = SerialisedMessage(json.dumps({"key": "value"}))
+    assert serialised_msg.deserialise()["key"] == "value"
 
 
 def test_deserialise_datetime() -> None:
     """Checks that datetimes can be correctly serialised"""
     timestamp = datetime.datetime.fromordinal(100012)
-    data = deserialise(
-        serialise(
-            mrn="",
-            accession_number="",
-            study_datetime=timestamp,
-            procedure_occurrence_id="",
-            project_name="",
-            omop_es_timestamp=datetime.datetime.now(),  # noqa: DTZ005
-        )
+    msg = Message(
+        {
+            "mrn": "",
+            "accession_number": "",
+            "study_datetime": timestamp,
+            "procedure_occurrence_id": "",
+            "project_name": "",
+            "omop_es_timestamp": datetime.datetime.now(),  # noqa: DTZ005
+        }
     )
+    serialised_msg = msg.serialise()
+    data = serialised_msg.deserialise()
     assert data["study_datetime"] == timestamp
