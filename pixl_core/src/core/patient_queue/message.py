b9e4a592dd6ae22c5c1d7882f90f16771e165a6b
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-19 13:57:28 +0000

    Update docstring

--- pixl_core/src/core/patient_queue/message.py
+++ pixl_core/src/core/patient_queue/message.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""Utility functions"""
+"""Classes to represent messages in the patient queue."""
 
 import json
 import logging
