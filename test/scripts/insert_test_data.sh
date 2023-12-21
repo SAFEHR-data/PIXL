c8e15af41e3da5bfcbfd749afbce6c56fb1372d3
Stef Piatek <s.piatek@ucl.ac.uk>
2023-12-21 13:30:54 +0000

    Try quoting in post of dicom image

--- test/scripts/insert_test_data.sh
+++ test/scripts/insert_test_data.sh
@@ -23,5 +23,5 @@ insert into star.core_demographic(mrn_id, sex) values (1234, 'F');
 docker exec -it system-test-fake-star-db /bin/bash -c "psql -U postgres -d emap -c \"$_sql_command\"" || true
 
 # Uses an accession number of "123456789"
-curl -X POST -u orthanc:orthanc http://localhost:8043/instances \
+curl -X POST -u "orthanc:orthanc" "http://localhost:8043/instances" \
   --data-binary @"$SCRIPT_DIR/../data/test.dcm"
