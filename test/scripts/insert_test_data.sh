b0561bb88e10dcdd2dd0d5051a30e96f6c7b359c
Jeremy Stein <j.stein@ucl.ac.uk>
2024-01-17 15:07:25 +0000

    Don't allow DB setup to fail

--- test/scripts/insert_test_data.sh
+++ test/scripts/insert_test_data.sh
@@ -36,7 +36,8 @@ insert into star.lab_result(lab_result_id, lab_order_id, lab_test_definition_id,
 insert into star.lab_result(lab_result_id, lab_order_id, lab_test_definition_id, value_as_text)
     VALUES (78902, 56782, 6789, 'this is a radiology report 2');
 "
-docker exec -it system-test-fake-star-db /bin/bash -c "psql -U postgres -d emap -c \"$_sql_command\"" || true
+
+docker exec -it system-test-fake-star-db /bin/bash -c "psql -U postgres -d emap -c \"$_sql_command\""
 
 # Uses an accession number of "AA12345601" for MRN 987654321
 curl -X POST -u "orthanc:orthanc" "http://localhost:8043/instances" \
