744ff6eae6db83d7075217f734998de405ebbe54
Jeremy Stein <j.stein@ucl.ac.uk>
2024-01-24 14:18:05 +0000

    wrong dir

--- test/run-system-test.sh
+++ test/run-system-test.sh
@@ -40,7 +40,7 @@ pixl extract-radiology-reports "${PACKAGE_DIR}/test/resources/omop"
 ./scripts/check_radiology_parquet.py \
   ../exports/test-extract-uclh-omop-cdm/all_extracts/omop/2023-12-07t14-08-58/radiology/radiology.parquet
 
-ls -laR exports/
+ls -laR ../exports/
 docker exec system-test-ehr-api rm -r /run/exports/test-extract-uclh-omop-cdm/
 
 cd "${PACKAGE_DIR}"
