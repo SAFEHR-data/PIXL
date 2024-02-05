bfa99982cb3f066d7c2ed12da50ab8f97b938741
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-05 16:08:08 +0100

    Remove unused import

--- test/scripts/check_ftps_upload.py
+++ test/scripts/check_ftps_upload.py
@@ -12,7 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import subprocess
+
 from pathlib import Path
 from shutil import rmtree
 from time import sleep
