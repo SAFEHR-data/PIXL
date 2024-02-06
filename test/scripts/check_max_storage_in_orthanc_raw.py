48a22119af6f10b91828113ed885abd45e094da6
Jon Stutters <j.stutters@ucl.ac.uk>
2024-02-06 10:32:36 +0000

    Reformat

--- test/scripts/check_max_storage_in_orthanc_raw.py
+++ test/scripts/check_max_storage_in_orthanc_raw.py
@@ -34,11 +34,16 @@ from write_fake_dicoms import write_volume
 SECONDS_WAIT = 5
 
 config = dotenv_values(".env.test")
-raw_instances_url = "http://localhost:{0}/instances".format(config["ORTHANC_RAW_WEB_PORT"])
+raw_instances_url = "http://localhost:{0}/instances".format(
+    config["ORTHANC_RAW_WEB_PORT"]
+)
 
 # Check we have the 2 instances expected from insert_test_data.sh
 for seconds in range(0, 121, SECONDS_WAIT):
-    original_instances = requests.get(raw_instances_url, auth=(config["ORTHANC_RAW_USERNAME"], config["ORTHANC_RAW_PASSWORD"])).json()
+    original_instances = requests.get(
+        raw_instances_url,
+        auth=(config["ORTHANC_RAW_USERNAME"], config["ORTHANC_RAW_PASSWORD"]),
+    ).json()
     print(f"Waited for {seconds} seconds, orthanc-raw instances: {original_instances}")
     if len(original_instances) == 2:
         break
@@ -50,7 +55,11 @@ with tempfile.TemporaryDirectory() as temp_dir:
     write_volume(temp_dir + "/dcm{slice:03d}.dcm")
     n_dcm = 0
     for dcm in Path(temp_dir).glob("*.dcm"):
-        upload_response = requests.post(raw_instances_url, auth=(config["ORTHANC_RAW_USERNAME"], config["ORTHANC_RAW_PASSWORD"]), files={"file": open(dcm, "rb")})
+        upload_response = requests.post(
+            raw_instances_url,
+            auth=(config["ORTHANC_RAW_USERNAME"], config["ORTHANC_RAW_PASSWORD"]),
+            files={"file": open(dcm, "rb")},
+        )
         if upload_response.status_code != 200:
             # orthanc will eventually refuse more instances becuase the test
             # exam we're using exceeds the max storage
@@ -60,10 +69,15 @@ with tempfile.TemporaryDirectory() as temp_dir:
 
 # Check the instances in orthanc-raw to see if the original instances have been removed
 for seconds in range(0, 121, SECONDS_WAIT):
-    new_instances = requests.get(raw_instances_url, auth=(config["ORTHANC_RAW_USERNAME"], config["ORTHANC_RAW_PASSWORD"])).json()
-    print("Waited for {seconds} seconds, orthanc-raw contains {n_instances} instances".format(
-        seconds=seconds, n_instances=len(new_instances)
-    ))
+    new_instances = requests.get(
+        raw_instances_url,
+        auth=(config["ORTHANC_RAW_USERNAME"], config["ORTHANC_RAW_PASSWORD"]),
+    ).json()
+    print(
+        "Waited for {seconds} seconds, orthanc-raw contains {n_instances} instances".format(
+            seconds=seconds, n_instances=len(new_instances)
+        )
+    )
     if any([instance in new_instances for instance in original_instances]):
         sleep(SECONDS_WAIT)
     else:
