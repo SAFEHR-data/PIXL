2764115e805d268694737ec36a3edc0edd64166c
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-08 11:19:49 +0000

    Use config to set env vars in core.upload

--- pixl_core/src/core/upload.py
+++ pixl_core/src/core/upload.py
@@ -17,11 +17,12 @@ from __future__ import annotations
 
 import ftplib
 import logging
-import os
 import ssl
 from datetime import datetime, timezone
 from ftplib import FTP_TLS
 from typing import TYPE_CHECKING, Any, BinaryIO
+
+from decouple import config
 
 if TYPE_CHECKING:
     from socket import socket
@@ -115,10 +116,10 @@ def upload_parquet_files(parquet_export: ParquetExport) -> None:
 
 def _connect_to_ftp() -> FTP_TLS:
     # Set your FTP server details
-    ftp_host = os.environ["FTP_HOST"]
-    ftp_port = os.environ["FTP_PORT"]  # FTPS usually uses port 21
-    ftp_user = os.environ["FTP_USER_NAME"]
-    ftp_password = os.environ["FTP_USER_PASSWORD"]
+    ftp_host = config("FTP_HOST")
+    ftp_port = config("FTP_PORT")  # FTPS usually uses port 21
+    ftp_user = config("FTP_USER_NAME")
+    ftp_password = config("FTP_USER_PASSWORD")
 
     # Connect to the server and login
     try:
