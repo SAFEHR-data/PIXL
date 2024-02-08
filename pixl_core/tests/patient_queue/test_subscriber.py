d3dbb8f9fccd97b174d1f974443fd5a7ca1e5a8e
ruaridhg <ruaridhg@users.noreply.github.com>
2024-02-08 14:10:59 +0000

    add additionally assert

--- pixl_core/tests/patient_queue/test_subscriber.py
+++ pixl_core/tests/patient_queue/test_subscriber.py
@@ -75,6 +75,7 @@ def test_consume_all() -> None:
         assert pp.connection_open
         assert pp.message_count == 0
         pp.publish(messages=[TEST_MESSAGE, TEST_MESSAGE])
+        assert pp.message_count == 1
 
     with PixlBlockingConsumer(queue_name=TEST_QUEUE) as bc:
         counter_bc = bc.consume_all(timeout_in_seconds=2, file_path=Path("test_producer.csv"))
