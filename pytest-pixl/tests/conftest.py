63695fad3a10d485f79df45c1ef596189ae7fbba
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-08 14:40:30 +0000

    Move test samples into `tests/` and ignore for pytest

--- pytest-pixl/tests/conftest.py
+++ pytest-pixl/tests/conftest.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 
+# Avoid running samples for fixture tests directly with pytest
+collect_ignore = ["samples_for_fixture_tests"]
+
 pytest_plugins = ["pytester"]
 
 TEST_DIR = Path(__file__).parent
@@ -9,5 +12,3 @@ os.environ["FTP_HOST"] = "localhost"
 os.environ["FTP_USER_NAME"] = "pixl_user"
 os.environ["FTP_USER_PASSWORD"] = "longpassword"  # noqa: S105 Hardcoding password
 os.environ["FTP_PORT"] = "20021"
-os.environ["FTP_CERT_FILE"] = f"{TEST_DIR}/ssl/localhost.crt"
-os.environ["FTP_KEY_FILE"] = f"{TEST_DIR}/ssl/localhost.key"
--- pytest-pixl/tests/test_ftpserver_fixture.py
+++ pytest-pixl/tests/test_ftpserver_fixture.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.mark.pytester_example_path("samples_for_fixture_tests/test_ftpserver_fixture")
+@pytest.mark.pytester_example_path("tests/samples_for_fixture_tests/test_ftpserver_fixture")
 def test_ftpserver_connection(pytester):
     """Test whether we can connect to the FTP server fixture"""
     pytester.copy_example("test_ftpserver_login.py")
