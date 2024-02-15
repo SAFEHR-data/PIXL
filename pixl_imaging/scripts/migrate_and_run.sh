4c484cf735017f9eb63527413c77a74ec14cfb54
Stef Piatek <s.piatek@ucl.ac.uk>
2024-02-15 09:35:17 +0000

    Use alembic config within alembic dir

--- pixl_imaging/scripts/migrate_and_run.sh
+++ pixl_imaging/scripts/migrate_and_run.sh
@@ -17,7 +17,7 @@
 # Run migrations (unless SKIP_ALEMBIC is true) then run the app.
 
 set -eux -o pipefail
-cd /app
+cd /app/alembic
 if [ "${SKIP_ALEMBIC:-false}" = false ]; then
     echo "Running alembic migrations"
     alembic upgrade head
