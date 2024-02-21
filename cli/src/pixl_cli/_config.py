f90f4a9c2ece88591c31bfb332dd60f9d572d0ca
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-21 11:50:12 +0000

    Build config objects in place

--- cli/src/pixl_cli/_config.py
+++ cli/src/pixl_cli/_config.py
@@ -17,37 +17,31 @@ from typing import Optional
 
 from decouple import config
 
-API_SETTINGS = {"ehr_api": {}, "imaging_api": {}}  # type: dict
-API_SETTINGS["ehr_api"]["host"] = config("PIXL_EHR_API_HOST", default="localhost")
-API_SETTINGS["ehr_api"]["port"] = int(config("PIXL_EHR_API_PORT", default=7006))
-API_SETTINGS["ehr_api"]["default_rate"] = int(config("PIXL_EHR_API_RATE", default=1))
-API_SETTINGS["imaging_api"]["host"] = config("PIXL_IMAGING_API_HOST", default="localhost")
-API_SETTINGS["imaging_api"]["port"] = int(config("PIXL_IMAGING_API_PORT", default=7007))
-API_SETTINGS["imaging_api"]["default_rate"] = int(config("PIXL_IMAGING_API_RATE", default=1))
-
-SERVICE_SETTINGS = {"rabbitmq": {}, "postgres": {}}  # type: dict
-SERVICE_SETTINGS["rabbitmq"]["host"] = config("RABBITMQ_HOST", default="localhost")
-SERVICE_SETTINGS["rabbitmq"]["port"] = int(config("RABBITMQ_PORT", default=7008))
-SERVICE_SETTINGS["rabbitmq"]["username"] = config("RABBITMQ_USERNAME", default="rabbitmq_username")
-SERVICE_SETTINGS["rabbitmq"]["password"] = config("RABBITMQ_PASSWORD", default="rabbitmq_password")
-
-SERVICE_SETTINGS["postgres"]["host"] = config("POSTGRES_HOST", default="localhost")
-SERVICE_SETTINGS["postgres"]["port"] = int(config("POSTGRES_PORT", default=7001))
-SERVICE_SETTINGS["postgres"]["username"] = config("PIXL_DB_USER", default="pixl_db_username")
-SERVICE_SETTINGS["postgres"]["password"] = config("PIXL_DB_PASSWORD", default="pixl_db_password")
-SERVICE_SETTINGS["postgres"]["database"] = config("PIXL_DB_NAME", default="pixl")
+SERVICE_SETTINGS = {
+    "rabbitmq": {
+        "host": config("RABBITMQ_HOST", default="localhost"),
+        "port": int(config("RABBITMQ_PORT", default=7008)),
+        "username": config("RABBITMQ_USERNAME", default="rabbitmq_username"),
+        "password": config("RABBITMQ_PASSWORD", default="rabbitmq_password"),
+    },
+    "postgres": {
+        "host": config("POSTGRES_HOST", default="localhost"),
+        "port": int(config("POSTGRES_PORT", default=7001)),
+        "username": config("PIXL_DB_USER", default="pixl_db_username"),
+        "password": config("PIXL_DB_PASSWORD", default="pixl_db_password"),
+        "database": config("PIXL_DB_NAME", default="pixl"),
+    },
+}  # type: dict
 
 
 class APIConfig:
     """API Configuration"""
 
-    def __init__(self, kwargs: dict) -> None:
+    def __init__(self, host: str, port: int, default_rate: Optional[int] = None) -> None:
         """Initialise the APIConfig class"""
-        self.host: Optional[str] = None
-        self.port: Optional[int] = None
-        self.default_rate: Optional[int] = None
-
-        self.__dict__.update(kwargs)
+        self.host = host
+        self.port = port
+        self.default_rate = default_rate
 
     @property
     def base_url(self) -> str:
@@ -55,15 +49,30 @@ class APIConfig:
         return f"http://{self.host}:{self.port}"
 
 
+_ehr_api_config = APIConfig(
+    host=config("PIXL_EHR_API_HOST", default="localhost"),
+    port=int(config("PIXL_EHR_API_PORT", default=7006)),
+    default_rate=int(config("PIXL_EHR_API_RATE", default=1)),
+)
+
+_imaging_api_config = APIConfig(
+    host=config("PIXL_IMAGING_API_HOST", default="localhost"),
+    port=int(config("PIXL_IMAGING_API_PORT", default=7007)),
+    default_rate=int(config("PIXL_IMAGING_API_RATE", default=1)),
+)
+
+API_CONFIGS = {"ehr": _ehr_api_config, "imaging": _imaging_api_config}
+
+
 def api_config_for_queue(queue_name: str) -> APIConfig:
     """Configuration for an API associated with a queue"""
     api_name = f"{queue_name}_api"
 
-    if api_name not in API_SETTINGS:
+    if api_name not in API_CONFIGS:
         msg = (
             f"Cannot update the rate for {queue_name}. {api_name} was"
             f" not specified in the configuration"
         )
         raise ValueError(msg)
 
-    return APIConfig(API_SETTINGS[api_name])
+    return API_CONFIGS[api_name]
