b1b5491aaaa8136a04011d88f600522e8ae97fd3
Jeremy Stein <j.stein@ucl.ac.uk>
2024-02-22 12:32:50 +0000

    Further linting fixes

--- test/test_max_storage_in_orthanc_raw.py
+++ test/test_max_storage_in_orthanc_raw.py
@@ -22,7 +22,7 @@ images are no longer stored.
 
 Polling to allow for orthanc processing time.
 """
-
+import logging
 import tempfile
 from pathlib import Path
 from time import sleep
@@ -34,7 +34,7 @@ from pytest_pixl.dicom import write_volume
 
 SECONDS_WAIT = 5
 
-raw_instances_url = "http://localhost:{0}/instances".format(config("ORTHANC_RAW_WEB_PORT"))
+raw_instances_url = "http://localhost:{}/instances".format(config("ORTHANC_RAW_WEB_PORT"))
 
 
 @pytest.mark.usefixtures("_setup_pixl_cli")
@@ -50,8 +50,11 @@ def test_max_storage_in_orthanc_raw():
         original_instances = requests.get(
             raw_instances_url,
             auth=(config("ORTHANC_RAW_USERNAME"), config("ORTHANC_RAW_PASSWORD")),
+            timeout=10,
         ).json()
-        print(f"Waited for {seconds} seconds, orthanc-raw instances: {original_instances}")
+        logging.info(
+            "Waited for %s seconds, orthanc-raw instances: %s", seconds, original_instances
+        )
         if len(original_instances) == 2:
             break
         sleep(SECONDS_WAIT)
@@ -64,34 +67,39 @@ def test_max_storage_in_orthanc_raw():
         for dcm in Path(temp_dir).glob("*.dcm"):
             # We use data= rather than files= in this request because orthanc does not return JSON
             # when files= is used.
-            upload_response = requests.post(
-                raw_instances_url,
-                auth=(config("ORTHANC_RAW_USERNAME"), config("ORTHANC_RAW_PASSWORD")),
-                data=open(dcm, "rb"),
-            )
+            with dcm.open("rb") as dcm_fh:
+                upload_response = requests.post(
+                    raw_instances_url,
+                    auth=(config("ORTHANC_RAW_USERNAME"), config("ORTHANC_RAW_PASSWORD")),
+                    data=dcm_fh,
+                    timeout=10,
+                )
             if upload_response.status_code != requests.codes.ok:
                 # orthanc will eventually refuse more instances because the test
                 # exam we're using exceeds the max storage
                 if upload_response.json()["OrthancError"] == "The file storage is full":
                     # This is what we're looking for when storage limit reached
                     break
-                else:
-                    # Something else happened preventing the upload
-                    raise (RuntimeError(f"Failed to upload {dcm} to orthanc-raw"))
+                # Something else happened preventing the upload
+                err_str = f"Failed to upload {dcm} to orthanc-raw"
+                raise RuntimeError(err_str)
             n_dcm += 1
-        print(f"Uploaded {n_dcm} new instances")
+        logging.info("Uploaded %s new instances", n_dcm)
 
     # Check the instances in orthanc-raw to see if the original instances have been removed
     for seconds in range(0, 121, SECONDS_WAIT):
         new_instances = requests.get(
             raw_instances_url,
             auth=(config("ORTHANC_RAW_USERNAME"), config("ORTHANC_RAW_PASSWORD")),
+            timeout=10,
         ).json()
-        print(f"Waited for {seconds} seconds, orthanc-raw contains {len(new_instances)} instances")
-        if any([instance in new_instances for instance in original_instances]):
+        logging.info(
+            "Waited for %s seconds, orthanc-raw contains %s instances", seconds, len(new_instances)
+        )
+        if any(instance in new_instances for instance in original_instances):
             sleep(SECONDS_WAIT)
         else:
-            print("Original instances have been removed from orthanc-raw")
+            logging.info("Original instances have been removed from orthanc-raw")
             break
 
-    assert not any([instance in new_instances for instance in original_instances])
+    assert not any(instance in new_instances for instance in original_instances)
