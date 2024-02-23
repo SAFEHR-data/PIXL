6dba81ed3b73ac571684b7f5713ef250fb545a18
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-23 02:13:38 +0000

    Create AzureKeyVault class to handle project secrets fetching

--- pixl_core/src/core/_secrets.py
+++ pixl_core/src/core/_secrets.py
@@ -14,35 +14,66 @@
 
 """Handles fetching of project secrets from the Azure Keyvault"""
 
+import subprocess
+
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from decouple import config
 
-AZURE_KEY_VAULT_NAME = config("EXPORT_AZ_KEY_VAULT_NAME")
+
+class AzureKeyVault:
+    """Handles fetching of project secrets from the Azure Keyvault"""
+
+    def __init__(self) -> None:
+        """
+        Initialise the AzureKeyVault instance
+
+        Creates an EnvironmentCredential via AzureDefaultCredential to connect with a
+        ServicePrincipal and secret configured via environment variables.
+
+        This requires the following environment variables to be set, which will be picked up by the
+        Azure SDK:
+        - AZURE_CLIENT_ID
+        - AZURE_CLIENT_SECRET
+        - AZURE_TENANT_ID
+        - AZURE_KEY_VAULT_NAME
+        """
+        self._check_envvars()
+        self.kv_name = config("EXPORT_AZ_KEY_VAULT_NAME")
+        self.client = self._connect_to_keyvault()
+
+    def _connect_to_keyvault(self) -> SecretClient:
+        key_vault_uri = f"https://{self.kv_name}.vault.azure.net"
+
+        credentials = DefaultAzureCredential()
+        return SecretClient(vault_url=key_vault_uri, credential=credentials)
+
+    def _check_envvars(self) -> None:
+        """
+        Check if the required environment variables are set.
+        These need to be set system-wide, as the Azure SDK picks them up from the environment.
+        :raises OSError: if any of the environment variables are not set
+        """
+        _check_system_envvar("AZURE_CLIENT_ID")
+        _check_system_envvar("AZURE_CLIENT_SECRET")
+        _check_system_envvar("AZURE_TENANT_ID")
+        _check_system_envvar("AZURE_KEY_VAULT_NAME")
+
+    def fetch_secret(self, secret_name: str) -> str:
+        """
+        Fetch a secret from the Azure Key Vault instance specified in the environment variables.
+        :return: the requested secret's value
+        """
+        secret = self.client.get_secret(secret_name).value
+        if secret is None:
+            msg = "Azure Key Vault secret is None"
+            raise ValueError(msg)
+
+        return str(secret)
 
 
-def _fetch_secret(secret_name: str) -> str:
-    """
-    Fetch a secret from the Azure Key Vault instance specified in the environment variables.
-    Creates an EnvironmentCredential via AzureDefaultCredential to connect with a
-    ServicePrincipal and secret configured via environment variables.
-
-    This requires the following environment variables to be set, which will be picked up by the
-    Azure SDK:
-    - AZURE_CLIENT_ID
-    - AZURE_CLIENT_SECRET
-    - AZURE_TENANT_ID
-    - AZURE_KEY_VAULT_NAME
-
-    :return: the requested secret's value
-    """
-    key_vault_uri = f"https://{AZURE_KEY_VAULT_NAME}.vault.azure.net"
-    credentials = DefaultAzureCredential()
-    client = SecretClient(vault_url=key_vault_uri, credential=credentials)
-
-    secret = client.get_secret(secret_name).value
-    if secret is None:
-        msg = "Azure Key Vault secret is None"
-        raise ValueError(msg)
-
-    return str(secret)
+def _check_system_envvar(var_name: str) -> None:
+    """Check if an environment variable is set system-wide"""
+    error_msg = f"Environment variable {var_name} not set"
+    if not subprocess.check_output(f"echo ${var_name}", shell=True).decode().strip():  # noqa: S602
+        raise OSError(error_msg)
