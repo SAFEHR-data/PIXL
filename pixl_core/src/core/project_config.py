155d60ab27429e1c3173c60963f66c70495b8483
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-26 19:41:41 +0100

    Load local `.env` if it exists before setting `PROJECT_CONFIGS_DIR`

--- pixl_core/src/core/project_config.py
+++ pixl_core/src/core/project_config.py
@@ -21,9 +21,12 @@ from pathlib import Path
 from typing import Any
 
 import yaml
-from decouple import config
+from decouple import Config, RepositoryEmpty, RepositoryEnv
 from pydantic import BaseModel, validator
 
+# Make sure local .env file is loaded if it exists
+env_file = Path.cwd() / ".env"
+config = Config(RepositoryEnv(env_file)) if env_file.exists() else Config(RepositoryEmpty())
 PROJECT_CONFIGS_DIR = Path(config("PROJECT_CONFIGS_DIR"))
 
 logger = logging.getLogger(__name__)
