795060d58b80a226ffbaa84892185a6c6fa13104
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-28 14:42:22 +0000

    Exports are now under `projects/exports`

--- test/conftest.py
+++ test/conftest.py
@@ -25,7 +25,7 @@ pytest_plugins = "pytest_pixl"
 @pytest.fixture()
 def host_export_root_dir():
     """Intermediate export dir as seen from the host"""
-    return Path(__file__).parents[1] / "exports"
+    return Path(__file__).parents[1] / "projects" / "exports"
 
 
 TEST_DIR = Path(__file__).parent
