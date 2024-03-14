7116500aaa8919e47467d9ab0689d23d1ba186fa
Jeremy Stein <j.stein@ucl.ac.uk>
2024-03-14 18:51:23 +0000

    TEMP - a bit of debugging to investigate the setup on GITHUB actions when it comes to permissions and how sudo works.

--- test/conftest.py
+++ test/conftest.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 """System/E2E test setup"""
+import os
+import subprocess
 from pathlib import Path
 
 import pytest
@@ -22,7 +24,7 @@ from utils import wait_for_stable_orthanc_anon
 pytest_plugins = "pytest_pixl"
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def host_export_root_dir():
     """Intermediate export dir as seen from the host"""
     return Path(__file__).parents[1] / "projects" / "exports"
@@ -34,8 +36,28 @@ RESOURCES_OMOP_DIR = RESOURCES_DIR / "omop"
 
 
 @pytest.fixture(scope="session")
-def _setup_pixl_cli(ftps_server) -> None:
+def _setup_pixl_cli(ftps_server, host_export_root_dir) -> None:
     """Run pixl populate/start. Cleanup intermediate export dir on exit."""
+    # see if it's already created
+    try:
+        print("JES: trying first ls -laR")
+        run_subprocess(["ls", "-laR", host_export_root_dir.parents[0]])
+    except subprocess.CalledProcessError:
+        try:
+            print("JES: trying second ls -laR")
+            run_subprocess(["ls", "-laR", host_export_root_dir.parents[1]])
+        except subprocess.CalledProcessError:
+            pass
+
+    # In production, it will be documented that the exports directory must be writable
+    # by the "pixl" user. For the system test we must do it here.
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        run_subprocess(
+            ["sudo", "--non-interactive", "chown", "-R", "pixl:pixl", host_export_root_dir]
+        )
+
+    print("JES: after wards ls -laR")
+    run_subprocess(["ls", "-laR", host_export_root_dir.parents[0]])
     # CLI calls need to have CWD = test dir so they can find the pixl_config.yml file
     run_subprocess(["pixl", "populate", str(RESOURCES_OMOP_DIR.absolute())], TEST_DIR)
     # poll here for two minutes to check for imaging to be processed, printing progress
