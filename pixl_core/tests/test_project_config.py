2aea37be317bea4f400c8cace7200d5e0a70ded5
Jeremy Stein <j.stein@ucl.ac.uk>
2024-03-22 17:26:35 +0000

    The "correct" test data wasn't being validated to ensure it was actually correct, thus some bugs in both the tests and the code were being hidden. Have fixed the optional fields and file existence validation. As a bonus, let's check all production configs are valid.

--- pixl_core/tests/test_project_config.py
+++ pixl_core/tests/test_project_config.py
@@ -37,36 +37,59 @@ def test_config_from_file():
     assert project_config.destination.parquet == "ftps"
 
 
-BASE_YAML_DATA = {
-    "project": {"name": "myproject", "modalities": ["DX", "CR"]},
-    "tag_operations": ["test-extract-uclh-omop-cdm.yaml"],
-    "destination": {"dicom": "ftps", "parquet": "ftps"},
-}
+@pytest.fixture()
+def base_yaml_data():
+    """Good data (excluding optional fields)"""
+    return {
+        "project": {"name": "myproject", "modalities": ["DX", "CR"]},
+        "tag_operation_files": ["test-extract-uclh-omop-cdm.yaml"],
+        "destination": {"dicom": "ftps", "parquet": "ftps"},
+    }
 
 
-def test_parquet_dicom_fails():
+def test_base_is_valid(base_yaml_data):
+    """Before anything else, check that the unaltered baseline validates ok."""
+    PixlConfig.model_validate(base_yaml_data)
+
+
+def test_parquet_dicom_fails(base_yaml_data):
     """
     Test that the config validation fails for non-valid values: 'dicomweb' not allowed for
     parquet destionation
     """
-    config_data = BASE_YAML_DATA
+    config_data = base_yaml_data
     config_data["destination"]["parquet"] = "dicomweb"
     with pytest.raises(ValidationError):
         PixlConfig.model_validate(config_data)
 
 
-def test_invalid_destinations():
+def test_invalid_destinations(base_yaml_data):
     """Test that the config validation fails for invalid destinations."""
-    config_data = BASE_YAML_DATA
+    config_data = base_yaml_data
     config_data["destination"]["dicom"] = "nope"
     config_data["destination"]["parquet"] = "nope"
     with pytest.raises(ValidationError):
         PixlConfig.model_validate(config_data)
 
 
-def test_invalid_paths():
+def test_too_many_paths(base_yaml_data):
+    """Test that the config validation fails if there are >1 tag operation files"""
+    config_data_extra_file = base_yaml_data
+    tof = config_data_extra_file["tag_operation_files"]
+    tof.append(tof[0])
+    with pytest.raises(ValidationError, match="at most 1"):
+        PixlConfig.model_validate(config_data_extra_file)
+
+
+def test_invalid_paths(base_yaml_data):
     """Test that the config validation fails for invalid tag-operation paths."""
-    config_data_wrong_base = BASE_YAML_DATA
-    config_data_wrong_base["tag_operations"][0] = "/i/dont/exist.yaml"
+    config_data_wrong_base = base_yaml_data
+    config_data_wrong_base["tag_operation_files"][0] = "/i/dont/exist.yaml"
     with pytest.raises(ValidationError):
         PixlConfig.model_validate(config_data_wrong_base)
+
+
+@pytest.mark.parametrize(("yaml_file"), PROJECT_CONFIGS_DIR.glob("*.yaml"))
+def test_all_real_configs(yaml_file):
+    """Test that all production configs are valid"""
+    _load_and_validate(yaml_file)
