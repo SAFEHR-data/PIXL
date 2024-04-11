9dc0b8ee670992962a5389e8de016e3c6feb4e7f
Milan Malfait <m.malfait@ucl.ac.uk>
2024-04-11 14:16:36 +0100

    Mock Hasher class for tests

--- hasher/tests/conftest.py
+++ hasher/tests/conftest.py
@@ -14,8 +14,13 @@
 from __future__ import annotations
 
 import os
+from typing import TYPE_CHECKING
 
 import pytest
+from hasher.hashing import Hasher  # type: ignore [import-untyped]
+
+if TYPE_CHECKING:
+    from pytest_pixl.keyvault import MockKeyVault  # type: ignore [import-untyped]
 
 os.environ["ENV"] = "test"
 
@@ -23,6 +28,39 @@ os.environ["ENV"] = "test"
 @pytest.fixture()
 def _dummy_key(monkeypatch):  # noqa: ANN202
     """Fixture to set up a dummy key to use for hashing tests"""
-    import hasher.hashing
+    import hasher.hashing  # type: ignore [import-untyped]
 
     monkeypatch.setattr(hasher.hashing, "fetch_key_from_vault", lambda: "test-key")
+
+
+@pytest.fixture(scope="module")
+def monkeymodule():
+    """Module level monkey patch."""
+    from _pytest.monkeypatch import MonkeyPatch
+
+    monkeypatch = MonkeyPatch()
+    yield monkeypatch
+    monkeypatch.undo()
+
+
+class MockHasher(Hasher):
+    """Mock the Hasher clas without setting a connection to the Azure Key Vault instance."""
+
+    def __init__(self, keyvault: MockKeyVault) -> None:
+        """
+        Initialise the Hasher instance without setting up the connection to the Azure Key Vault
+        instance.
+        """
+        self.keyvault = keyvault
+
+
+@pytest.fixture(autouse=True, scope="module")
+def mock_hasher(monkeymodule, azure_keyvault) -> Hasher:
+    """
+    Mock the Hasher class as a pytest fixture.
+    Uses the azure_kevyault fixture from pytest_pixl to mock the Azure Key Vault instance.
+    """
+    hasher = MockHasher(azure_keyvault)
+    monkeymodule.setattr("hasher.hashing", hasher)
+
+    return hasher
