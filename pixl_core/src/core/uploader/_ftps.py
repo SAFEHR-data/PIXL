c3a17393caf4410e7d7f9a05c12bc3929dff082f
Milan Malfait <m.malfait@ucl.ac.uk>
2024-04-22 11:21:26 +0100

    Better docstring for `core.uploader._ftps`

--- pixl_core/src/core/uploader/_ftps.py
+++ pixl_core/src/core/uploader/_ftps.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""Helper functions for setting up a connection to an FTPS server."""
+"""Uploader subclass for FTPS uploads."""
 
 from __future__ import annotations
 
