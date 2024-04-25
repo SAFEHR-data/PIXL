fbd282a96361d1200b5357c7a3afcbc25e49a632
Milan Malfait <m.malfait@ucl.ac.uk>
2024-04-25 16:52:11 +0100

    Slight docstring rewording
    Co-authored-by: Stef Piatek <s.piatek@ucl.ac.uk>

--- pixl_core/src/core/uploader/_ftps.py
+++ pixl_core/src/core/uploader/_ftps.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""Uploader subclass for FTPS uploads."""
+"""Uploader subclass for FTPS."""
 
 from __future__ import annotations
 
