be155761e423180286ea1b24299ac1c1ea078e06
Milan Malfait <m.malfait@ucl.ac.uk>
2024-04-29 13:51:45 +0200

    Add negative checks in case of wrong DICOMweb credentials

--- pixl_core/tests/uploader/test_dicomweb.py
+++ pixl_core/tests/uploader/test_dicomweb.py
@@ -76,18 +76,52 @@ def test_dicomweb_server_config(run_containers, dicomweb_uploader) -> None:
     assert "test" in servers_response.json()
 
 
-def test_upload_dicom_image(study_id, run_containers, dicomweb_uploader) -> None:
-    """Tests that DICOM image can be uploaded to a DICOMWeb server"""
-    stow_response = dicomweb_uploader.send_via_stow(study_id)
-
-    # Check that the instance has arrived on the DICOMweb server
-    time.sleep(2)
-    studies_response = requests.get(
+def _check_study_present_on_dicomweb(study_id: str) -> bool:
+    """Check if a study is present on the DICOMWeb server."""
+    response = requests.get(
         LOCAL_DICOMWEB_URL + "/studies",
         auth=(DICOMWEB_USERNAME, DICOMWEB_PASSWORD),
         timeout=30,
     )
+    return any(study == study_id for study in response.json())
 
+
+def test_upload_dicom_image(study_id, run_containers, dicomweb_uploader) -> None:
+    """Tests that DICOM image can be uploaded to a DICOMWeb server"""
+    stow_response = dicomweb_uploader.send_via_stow(study_id)
     assert stow_response.status_code == 200  # succesful upload
-    assert studies_response.status_code == 200
-    assert len(studies_response.json()) == 1
+
+    # Check that the instance has arrived on the DICOMweb server
+    time.sleep(2)
+    assert _check_study_present_on_dicomweb(study_id)
+
+    # Clean up
+    requests.delete(
+        LOCAL_DICOMWEB_URL + "/studies/" + study_id,
+        auth=(DICOMWEB_USERNAME, DICOMWEB_PASSWORD),
+        timeout=30,
+    )
+
+
+def test_dicomweb_upload_fails_with_wrong_credentials(
+    study_id, run_containers, dicomweb_uploader
+) -> None:
+    """Tests that the DICOMWeb uploader fails when given wrong credentials."""
+    dicomweb_uploader.endpoint_user = "wrong"
+    dicomweb_uploader.endpoint_password = "wrong"  # noqa: S105, hardcoded password
+
+    # Force updating of the credentials, so we don't need to restart the container
+    dicomweb_uploader._setup_dicomweb_credentials()  # noqa: SLF001, private method
+
+    dicomweb_uploader.send_via_stow(study_id)
+    assert not _check_study_present_on_dicomweb(study_id)
+
+
+def test_dicomweb_upload_fails_with_wrong_url(study_id, run_containers, dicomweb_uploader) -> None:
+    """Tests that the DICOMWeb uploader fails when given wrong URL."""
+    dicomweb_uploader.endpoint_url = "wrong"
+    # Force updating of the credentials, so we don't need to restart the container
+    dicomweb_uploader._setup_dicomweb_credentials()  # noqa: SLF001, private method
+
+    dicomweb_uploader.send_via_stow(study_id)
+    assert not _check_study_present_on_dicomweb(study_id)
