53fb47f593023cfd10b845ea0b4dc9fe9aab5f79
Milan Malfait <m.malfait@ucl.ac.uk>
2024-04-29 16:23:41 +0200

    Fix system test: actually check for presence of studies on DICOMweb

--- test/system_test.py
+++ test/system_test.py
@@ -109,7 +109,10 @@ class TestFtpsUpload:
             self._check_dcm_tags_from_zip(z, unzip_dir, expected_studies)
 
     def _check_dcm_tags_from_zip(
-        self, zip_path: Path, unzip_dir: Path, expected_studies: dict[str, set[tuple[str, str]]]
+        self,
+        zip_path: Path,
+        unzip_dir: Path,
+        expected_studies: dict[str, set[tuple[str, str]]],
     ) -> None:
         """Check that private tag has survived anonymisation with the correct value."""
         expected_instances = expected_studies[zip_path.stem]
@@ -176,16 +179,26 @@ def test_ehr_anon_entries() -> None:
 def test_dicomweb_upload() -> None:
     """Check upload to DICOMweb server was successful"""
     # This should point to the orthanc-anon server
-    ORTHANC_URL = "http://localhost:7003"
+    LOCAL_DICOMWEB_URL = "http://localhost:8044"
 
-    def check_dicomweb_study_present() -> bool:
+    dicomweb_studies: list[str] = []
+
+    def dicomweb_studies_list() -> str:
+        return f"DICOMweb studies found: {dicomweb_studies}"
+
+    def two_studies_present_on_dicomweb() -> bool:
+        nonlocal dicomweb_studies
         response = requests.get(
-            ORTHANC_URL + "/dicom-web/studies",
-            auth=("orthanc_anon_username", "orthanc_anon_password"),
-            data={"Uri": "/instances"},
+            LOCAL_DICOMWEB_URL + "/studies",
+            auth=("orthanc_dicomweb", "orthanc_dicomweb"),
             timeout=30,
         )
-        # Taken from https://orthanc.uclouvain.be/hg/orthanc-dicomweb/file/default/Resources/Samples/Python/SendStow.py
-        return response.status_code == 200 and "00081190" in response.json()[0]
+        dicomweb_studies = response.json()
+        return len(dicomweb_studies) == 2
 
-    wait_for_condition(check_dicomweb_study_present)
+    wait_for_condition(
+        two_studies_present_on_dicomweb,
+        seconds_max=121,
+        seconds_interval=10,
+        progress_string_fn=dicomweb_studies_list,
+    )
