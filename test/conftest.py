b527c4507a6e1a3a20fa9bf9b6576df76304fecb
Stef Piatek <s.piatek@ucl.ac.uk>
2024-05-29 13:35:01 +0100

    Increase waiting time, as we now have to wait for an export

--- test/conftest.py
+++ test/conftest.py
@@ -147,7 +147,7 @@ def _setup_pixl_cli(ftps_server: PixlFTPServer, _populate_vna: None) -> Generato
     """Run pixl populate/start. Cleanup intermediate export dir on exit."""
     run_subprocess(["pixl", "populate", str(RESOURCES_OMOP_DIR.absolute())], TEST_DIR)
     # poll here for two minutes to check for imaging to be processed, printing progress
-    wait_for_images_to_be_exported(121, 5, 15)
+    wait_for_images_to_be_exported(181, 5, 15)
     yield
     run_subprocess(
         [
@@ -167,7 +167,7 @@ def _setup_pixl_cli_dicomweb(_populate_vna: None) -> Generator:
     """Run pixl populate/start. Cleanup intermediate export dir on exit."""
     run_subprocess(["pixl", "populate", str(RESOURCES_OMOP_DICOMWEB_DIR.absolute())], TEST_DIR)
     # poll here for two minutes to check for imaging to be processed, printing progress
-    wait_for_images_to_be_exported(121, 5, 15)
+    wait_for_images_to_be_exported(181, 5, 15)
     yield
     run_subprocess(
         [
