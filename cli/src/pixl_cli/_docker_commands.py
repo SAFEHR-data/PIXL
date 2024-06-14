e0347f7554b09d994e91d23a841afd9471678a5d
Milan Malfait <m.malfait@ucl.ac.uk>
2024-06-14 17:37:56 +0100

    Use `ENV` envvar to set docker project name

--- cli/src/pixl_cli/_docker_commands.py
+++ cli/src/pixl_cli/_docker_commands.py
@@ -18,19 +18,11 @@ from pathlib import Path
 from typing import Optional
 
 import click
+from decouple import config
 from loguru import logger
 
-ALLOWED_PROJECT_NAMES = ["pixl_dev", "pixl_test", "pixl_prod"]
 PIXL_ROOT = Path(__file__).parents[3].resolve()
 
-docker_project_option = click.option(
-    "-p",
-    "--project",
-    type=click.Choice(ALLOWED_PROJECT_NAMES, case_sensitive=False),
-    default="pixl_dev",
-    show_default=True,
-    help="Project to run the service for",
-)
 docker_env_option = click.option(
     "--env-file",
     type=click.Path(exists=True),
@@ -45,32 +37,28 @@ docker_extra_args = click.argument("extra_args", nargs=-1, type=click.UNPROCESSE
 # Required to allow passing unkown options to docker-compose
 # https://click.palletsprojects.com/en/8.1.x/advanced/#forwarding-unknown-options
 @click.command(context_settings={"ignore_unknown_options": True})
-@docker_project_option
 @docker_env_option
 @docker_extra_args
-def up(project: str, env_file: list[Path], *, extra_args: list) -> None:
+def up(env_file: list[Path], *, extra_args: list) -> None:
     """Start all the PIXL services"""
     # Construct the docker-compose arguments
     docker_args = ["up", "--wait", "--build", "--remove-orphans", *extra_args]
-    run_docker_compose(project, env_file, docker_args, working_dir=PIXL_ROOT)
+    run_docker_compose(env_file, docker_args, working_dir=PIXL_ROOT)
 
 
 # Required to allow passing unkown options to docker-compose
 # https://click.palletsprojects.com/en/8.1.x/advanced/#forwarding-unknown-options
 @click.command(context_settings={"ignore_unknown_options": True})
-@docker_project_option
 @docker_env_option
 @docker_extra_args
-def down(project: str, env_file: list[Path], *, extra_args: list) -> None:
+def down(env_file: list[Path], *, extra_args: list) -> None:
     """Stop all the PIXL services"""
     # Construct the docker-compose arguments
     docker_args = ["down", *extra_args]
-    run_docker_compose(project, env_file, docker_args, working_dir=PIXL_ROOT)
+    run_docker_compose(env_file, docker_args, working_dir=PIXL_ROOT)
 
 
-def run_docker_compose(
-    project: str, env_file: list[Path], args: list, working_dir: Optional[Path]
-) -> None:
+def run_docker_compose(env_file: list[Path], args: list, working_dir: Optional[Path]) -> None:
     """Wrapper to run docker-compose through the CLI."""
     docker_cmd = shutil.which("docker")
 
@@ -78,11 +66,13 @@ def run_docker_compose(
         err_msg = "docker not found in $PATH. Please make sure it's installed."
         raise FileNotFoundError(err_msg)
 
+    pixl_env = config("ENV")
+
     docker_args = [
         docker_cmd,
         "compose",
         "--project-name",
-        project,
+        f"pixl_{pixl_env}",
         # env_file will be a list of paths, so we need to flatten it
         *[f"--env-file={f}" for f in env_file],
         *args,
