16d9beef7ed9addbf4ef17f4763779159276d33c
Paul Smith <paul.j.smith@ucl.ac.uk>
2024-08-09 10:57:40 +0100

    Update retry_until_export_count_is_unchanged to handle multiple projects in CSV files

--- cli/tests/test_message_processing.py
+++ cli/tests/test_message_processing.py
@@ -40,17 +40,16 @@ def mock_publisher(mocker) -> Generator[Mock, None, None]:
 
 
 @pytest.mark.usefixtures("_zero_message_count")
-def test_no_retry_if_none_exported(example_messages, db_session, mock_publisher):
+def test_no_retry_if_none_exported(example_messages_df, db_session, mock_publisher):
     """
     GIVEN no images have been exported before starting, and num_retries set to 5
     WHEN rabbitmq messages set to zero and no messages are published to queue
     THEN populate_queue_and_db should never be called
     """
     os.environ["CLI_RETRY_SECONDS"] = "1"
-    project_name = example_messages[0].project_name
 
     retry_until_export_count_is_unchanged(
-        example_messages, num_retries=5, queues_to_populate=["imaging"], project_name=project_name
+        example_messages_df, num_retries=5, queues_to_populate=["imaging"]
     )
 
     mock_publisher.assert_not_called()
@@ -66,13 +65,11 @@ def test_retry_with_image_exported_and_no_change(
     THEN populate_queue_and_db should be called once
     """
     os.environ["CLI_RETRY_SECONDS"] = "1"
-    project_name = example_messages_df.iloc[0].project_name
 
     retry_until_export_count_is_unchanged(
         example_messages_df,
         num_retries=5,
         queues_to_populate=["imaging"],
-        project_name=project_name,
     )
 
     mock_publisher.assert_called_once()
