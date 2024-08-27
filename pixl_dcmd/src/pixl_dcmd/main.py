0203615f0d5dc688c9f25710c3d382984cac59eb
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2024-08-27 09:58:49 +0100

    adds missing varaible change for anonymised_study_info

--- pixl_dcmd/src/pixl_dcmd/main.py
+++ pixl_dcmd/src/pixl_dcmd/main.py
@@ -168,7 +168,7 @@ def anonymise_dicom(
         )
         anonymised_study_info = get_study_info(dataset)
         dataset[0x0010, 0x0020].value = get_pseudo_patient_id_and_update_db(
-            project_name, study_info, anonymise_study_info.mrn
+            project_name, study_info, anonymised_study_info.mrn
         )  # type: ignore[func-returns-value]
 
 
