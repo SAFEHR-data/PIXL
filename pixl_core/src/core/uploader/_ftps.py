f3d1ab15fac3cf25ef483fcae478954c3566e558
Stef Piatek <s.piatek@ucl.ac.uk>
2026-08-03 16:26:23 +0100

    tmp dir contents

--- pixl_core/src/core/uploader/_ftps.py
+++ pixl_core/src/core/uploader/_ftps.py
@@ -211,7 +211,9 @@ def _create_and_set_as_cwd_multi_path(ftp: FTP_TLS, remote_multi_dir: Path) -> N
 
 def _create_and_set_as_cwd(ftp: FTP_TLS, project_dir: str) -> None:
     logger.warning("cwd: {}", ftp.pwd())
-    logger.warning("contents: {}", ftp.list())
+    logger.warning("contents: {}", ftp.retrlines('LIST'))
+    ftp.cwd("ftp")
+
     try:
         ftp.mkd(project_dir)
     except ftplib.error_perm:
