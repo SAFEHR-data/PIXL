e5376c2ea2966ffa54804cbb9bdcc6a6042e0a79
Stefan Piatek <s.piatek@ucl.ac.uk>
2026-08-03 09:47:46 +0100

    Encapsulate compressed pixel data

--- pixl_dcmd/src/pixl_dcmd/main.py
+++ pixl_dcmd/src/pixl_dcmd/main.py
@@ -262,7 +262,7 @@ def _clean_dicom_image_pixels(
     burned_pixels = has_burned_pixels(dataset, deid=deid_recipe)
     cleaned_pixels = clean_pixel_data(dicom_file=dataset, results=burned_pixels)
 
-    dataset.PixelData = cleaned_pixels.tobytes()
+    dataset.PixelData = pydicom.encaps.encapsulate([cleaned_pixels.tobytes()])
 
 
 def _anonymise_dicom_from_scheme(
