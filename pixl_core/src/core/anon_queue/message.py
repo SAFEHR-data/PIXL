26ab1ef6bf2610a218d1c508adf2d81dd610d7be
ruaridhg <ruaridhg@users.noreply.github.com>
2026-08-21 09:57:01 +0100

    Add anon_queue to pixl_core with different message and no priority

---
+++ pixl_core/src/core/anon_queue/message.py
@@ -0,0 +1,65 @@
+#  Copyright (c) 2022 University College London Hospitals NHS Foundation Trust
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""Classes to represent messages in the patient queue."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+from jsonpickle import decode, encode
+from loguru import logger
+
+
+@dataclass
+class Message:
+    """
+    Representation of a RabbitMQ message containing the information
+    to identify an anonymisation request.
+    """
+
+    resource_id: str
+    study_uid: str
+    series_uid: str
+    project_name: str
+
+    @property
+    def identifier(self) -> str:
+        """Identifier for message"""
+        return (f"Message({self.resource_id=} {self.study_uid=} {self.series_uid=}").replace(
+            "self.", ""
+        )
+
+    def serialise(self, *, deserialisable: bool = True) -> bytes:
+        """
+        Serialise the message into a JSON string and convert to bytes.
+
+        :param deserialisable: If True, the serialised message will be deserialisable, by setting
+            the unpicklable flag to False in jsonpickle.encode(), meaning that the original Message
+            object can be recovered by `deserialise()`. If False, calling `deserialise()` on the
+            serialised message will return a dictionary.
+        """
+        logger.trace("Serialising {}", self)
+        return str.encode(encode(self, unpicklable=deserialisable))
+
+
+def deserialise(serialised_msg: bytes) -> Any:
+    """
+    Deserialise a message from a bytes-encoded JSON string.
+    If the message was serialised with `deserialisable=True`, the original Message object will be
+    returned. Otherwise, a dictionary will be returned.
+
+    :param serialised_msg: The serialised message.
+    """
+    return decode(serialised_msg)  # noqa: S301, since we control the input, so no security risks
---
+++ pixl_core/src/core/anon_queue/producer.py
@@ -0,0 +1,86 @@
+#  Copyright (c) 2022 University College London Hospitals NHS Foundation Trust
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""Producer for RabbitMQ"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from loguru import logger
+from opentelemetry import trace
+from pika import BasicProperties, DeliveryMode
+
+from ._base import PixlBlockingInterface
+
+if TYPE_CHECKING:
+    from core.anon_queue.message import Message
+
+tracer = trace.get_tracer("pixl_core.anon_queue.producer")
+
+
+class PixlProducer(PixlBlockingInterface):
+    """Generic publisher for RabbitMQ"""
+
+    def publish(self, messages: list[Message]) -> None:
+        """
+        Sends a list of serialised messages to a queue.
+        :param messages: list of messages to be sent to queue
+        """
+        if len(messages) == 0:
+            logger.warning("List of messages is empty so nothing will be published to queue.")
+            return
+
+        logger.info("Publishing {} messages to queue: {}", len(messages), self.queue_name)
+        for msg in messages:
+            attributes = {
+                "project_name": msg.project_name,
+                "resource_id": msg.resource_id,
+                "series_uid": msg.series_uid,
+                "study_uid": msg.study_uid,
+            }
+            with tracer.start_as_current_span("publish_message", attributes=attributes):
+                self._publish_message(msg)
+
+    def _publish_message(self, message: Message) -> None:
+        """
+        Publish a single serialised message to a queue.
+        :param message: message to be sent to queue
+        """
+        serialised_msg = message.serialise()
+        self._channel.basic_publish(
+            exchange="",
+            routing_key=self.queue_name,
+            body=serialised_msg,
+            properties=BasicProperties(
+                delivery_mode=DeliveryMode.Persistent,
+            ),
+        )
+
+        logger.bind(
+            project_name=message.project_name,
+            resource_id=message.resource_id,
+            series_uid=message.series_uid,
+            study_uid=message.study_uid,
+        ).debug(
+            "Message {} published to queue {}",
+            message,
+            self.queue_name,
+        )
+
+    def clear_queue(self) -> None:
+        """
+        Triggering a purge of all the messages currently in the queue. Mainly used to
+        clean after tests.
+        """
+        self._channel.queue_purge(queue=self.queue_name)
