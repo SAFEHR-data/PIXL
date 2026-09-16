fe71ad3641f41924f6e2e0e619d01a03d0dbd805
ruaridhg <ruaridhg@users.noreply.github.com>
2026-09-16 13:25:53 +0100

    Fix unit tests

--- pixl_core/tests/queue/test_subscriber.py
+++ pixl_core/tests/queue/test_subscriber.py
@@ -37,7 +37,7 @@ class ExpectedTestError(Exception):
 )
 async def test_create(mock_message) -> None:
     """Checks consume is working."""
-    with PixlProducer(queue_name=TEST_QUEUE) as producer:
+    with PixlProducer(queue_name=TEST_QUEUE, max_priority=5) as producer:
         producer.publish(messages=[mock_message], priority=1)
 
     consume = AsyncMock()
